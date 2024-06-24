e3af1f6ea5b18ce6dea14e1698969669e4e48dd5
sujeetghodke <sujeetghodke16@gmail.com>
2024-06-24 20:28:06 +0530

    index.js file added in reactRouter

---
+++ reactRouter/src/index.js
@@ -0,0 +1,7 @@
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Github from "./components/Github/Github";
+import Home from "./components/Home/Home";
+import User from "./components/User/User";
+
+export { About, Contact, Github, Home, User };
